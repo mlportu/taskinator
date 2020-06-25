a3adeb0d1f8d53469f6c889696a8e285bc7a42a3
Michael Portu <michaelportu@Michaels-MacBook-Pro.local>
2020-06-25 11:48:08 -0500

    editng tasks final

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,14 +16,40 @@ var taskFormHandler = function () {
         return false;
     }
     formEl.reset();
+    
+    var isEdit = formEl.hasAttribute("data-task-id");
+    
     //package up data as an object
     var taskDataObj = {
         name: taskNameInput,
         type: taskTypeInput,
     };
     //send it as an argument to createTaskEl
+    if(isEdit){
+        var taskId = formEl.getAttribute("data-task-id");
+        completeEditTask(taskNameInput, taskTypeInput, taskId);
+    }
+    else{
+        var taskDataObj = {
+            name:taskNameInput,
+            type: taskTypeInput,
+        };
     createTaskEl(taskDataObj);
+    }
 }
+var completeEditTask = function(taskName, taskType, taskId) {
+    //find the matching task list item
+    var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
+
+    //set new values
+    taskSelected.querySelector("h3.task-name").textContent=taskName;
+    taskSelected.querySelector("span.task-type").textContent=taskType;
+
+    alert("Task Updated!");
+
+    formEl.removeAttribute("data-task-id");
+    document.querySelector("#save-task").textContent = "Add Task";
+};
 
 var createTaskEl = function(taskDataObj){
 
