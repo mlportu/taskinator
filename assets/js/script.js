3fdfc2540b1b3e5dbce0b721a300d34ac5da4b14
Michael Portu <michaelportu@Michaels-MacBook-Pro.local>
2020-06-25 11:22:18 -0500

    edit task part 1

--- assets/js/script.js
+++ assets/js/script.js
@@ -89,13 +89,36 @@ var createTaskActions = function(taskIdCounter){
 }
 
 var taskButtonHandler = function(event){
-    console.log(event.target);
+    var targetEl = event.target;
 
-    if(event.target.matches(".delete-btn")){
+    //edit button clicked
+
+    if (targetEl.matches(".edit-btn")){
+        var taskId = targetEl.getAttribute("data-task-id");
+        editTask(taskId);
+    }
+    //delete button clicked
+    else if (targetEl.matches(".delete-btn")){
         var taskId = event.target.getAttribute("data-task-id");
         deleteTask(taskId);
     }
 };
+
+var editTask = function(taskId){
+    console.log("editing task #" + taskId);
+
+    var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId +"']");
+    var taskName = taskSelected.querySelector("h3.task-name").textContent;
+
+    var taskType = taskSelected.querySelector("span.task-type").textContent;
+
+    document.querySelector("input[name='task-name']").value=taskName;
+    document.querySelector("select[name='task-type']").value=taskType;
+    document.querySelector("#save-task").textContent="Save Task";
+
+    formEl.setAttribute("data-task-id", taskId);
+}
+
 var deleteTask = function(taskId) {
     var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
     taskSelected.remove();
