e3196095c38d32d837e03960ff592885577b7ae7
Michael Portu <michaelportu@Michaels-MacBook-Pro.local>
2020-06-27 10:00:08 -0500

    refactor code, adding local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -265,51 +265,20 @@ var saveTasks = function(){
 
 var loadTasks = function (){
     // retrieve tasks from local storage
-   var tasks = localStorage.getItem("tasks");
+   var savedTasks = localStorage.getItem("tasks");
 
-    if(tasks=== null){
-        tasks=[]; 
+    if(savedTasks === null){
         return false;
     }
 
-    tasks=JSON.parse(tasks);
+    savedTasks=JSON.parse(savedTasks);
 
-    for(var i=0; i<tasks.length; i++){
+    for(var i=0; i<savedTasks.length; i++){
         
-        tasks[i].id = taskIdCounter;
-
-        var  listItemEl= document.createElement("li");
-        listItemEl.className = "task-item";
-
-        listItemEl.setAttribute("data-task-id", tasks[i].id);
-        listItemEl.setAttribute("draggable","true");
-
-        var taskInfoEl = document.createElement("div");
-        taskInfoEl.className = "task-info";
-        taskInfoEl.innerHTML = "<h3 class='task-name'>" + tasks[i].name +"</h3><span class='task-type'>" + tasks[i].type + "</span>";
-        listItemEl.appendChild(taskInfoEl);
-
-        var taskActionsEl = createTaskActions(tasks[i].id);
-        listItemEl.appendChild(taskActionsEl);
+        createTaskEl(savedTasks[i]);
+    }   
     
-
-        if(tasks[i].status === "to do"){
-            listItemEl.querySelector("select[name='status-change']").selectedIndex = 0;
-            tasksToDoEl.appendChild(listItemEl);
-        }
-        else if(tasks[i].status === "in progress"){
-            listItemEl.querySelector("select[name='status-change']").selectedIndex = 1;
-            taskInProgressEl.appendChild(listItemEl);
-        }
-        else if(tasks[i].status === "complete"){
-            listItemEl.querySelector("select[name='status-change']").selectedIndex = 2;
-            taskCompletedEl.appendChild(listItemEl);
-        }
-    }
-    
-    taskIdCounter++;
-    
-    console.log(listItemEl);
+    console.log(savedTasks);
 }
 
 pageContentEl.addEventListener("click", taskButtonHandler);
